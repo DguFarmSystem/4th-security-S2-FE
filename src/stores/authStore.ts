d997499566022c5bd73aaee21a704cbd52e820fa
Cllaude99 <xodbsrla1025@naver.com>
2025-05-24 01:29:09 +0900

    feat: implement user store

---
+++ src/stores/authStore.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface AuthState {
+  accessToken: string;
+  isUnivAuthenticated: boolean;
+  isGuest: boolean;
+  hasVisited: boolean;
+  setAccessToken: (token: string) => void;
+  setIsUnivAuthenticated: (value: boolean) => void;
+  setIsGuest: (value: boolean) => void;
+  setHasVisited: (value: boolean) => void;
+  clearAuth: () => void;
+}
+
+const initialState = {
+  accessToken: '',
+  isUnivAuthenticated: false,
+  isGuest: false,
+  hasVisited: false,
+};
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      ...initialState,
+      setAccessToken: (token) => set({ accessToken: token }),
+      setIsUnivAuthenticated: (value) => set({ isUnivAuthenticated: value }),
+      setIsGuest: (value) => set({ isGuest: value }),
+      setHasVisited: (value) => set({ hasVisited: value }),
+      clearAuth: () => set(initialState),
+    }),
+    {
+      name: 'SOT-auth-storage',
+    }
+  )
+);

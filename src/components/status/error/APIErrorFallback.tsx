ff15ad7a92f93f5fdedfda223a38e0a58d45068c
Cllaude99 <xodbsrla1025@naver.com>
2025-05-23 22:07:42 +0900

    feat: clear auth-storage at 401 status

--- src/components/status/error/APIErrorFallback.tsx
+++ src/components/status/error/APIErrorFallback.tsx
@@ -3,16 +3,21 @@ import { isAxiosError } from 'axios';
 import { Navigate } from 'react-router-dom';
 import { getAPIErrorInfo } from '@/utils/getApiErrorInfo';
 import ErrorPage from './ErrorPage';
+import { PATH } from '@/constants/path';
+import { useAuthStore } from '@/stores/authStore';
 
 export const APIErrorFallback = ({
   error,
   resetErrorBoundary,
 }: FallbackProps) => {
+  const { clearAuth } = useAuthStore();
+
   if (isAxiosError(error)) {
     const errorInfo = getAPIErrorInfo(error);
 
     if (errorInfo.status === '401') {
-      return <Navigate to={''} replace />;
+      clearAuth();
+      return <Navigate to={PATH.WELCOME} replace />;
     }
 
     return (

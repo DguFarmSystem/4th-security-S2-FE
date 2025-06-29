4185875783381f13b3a585924185598e3e59777b
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-06-29 18:41:04 +0900

    feat: 폼 작성 페이지 질문 제외 레이아웃

--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -1,13 +1,63 @@
+import { useState, useEffect } from 'react';
+import IconBack from '@/assets/icons/IconBack.svg';
+
+const dummyData = {
+  title: "웹사이트 개발자 찾아요.",
+  date: "2025-05-17T12:00:00Z",
+  tags: ["유료 작업", "개발"],
+};
+
+function parseData(data: string) {
+  // yyyy-mm-dd를 mm.dd
+  const date = new Date(data);
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${month}.${day}`;
+}
+
 export default function FormPage() {
+  const [data, setData] = useState<typeof dummyData | null>(null);
+
+  useEffect(() => {
+      setData(dummyData);
+    }, []);
+  
+  if (!data) return null;
+
   return (
     <div className="w-full h-full">
       <form className='flex flex-col justify-between w-full h-full'>
-        <div className="flex">
-          <h2 className="text-xl font-bold text-[#F5179A]">
+        <div className="flex flex-col mt-[17px] gap-3">
+          <button
+            type="button"
+            onClick={() => window.history.back()}
+            className="flex items-center justify-center w-8 h-8 mx-[27px]"
+          >
+            <img
+              src={IconBack}
+              alt="뒤로가기"
+            />
+          </button>
+          {/* Title */}
+          <h2 className="text-xl font-bold text-[#F5179A] mx-[31px]">
             웹사이트 개발자 찾아요.
           </h2>
+
+          {/* Tags & Date */}
+          <div className="flex gap-1 mx-[31px]">
+            <div className="bg-[#B281F4] rounded-[3px] flex w-9 justify-center items-center">
+              <p className="text-[8px]">{data.tags[0]}</p>
+            </div>
+            <div className="bg-[#53BDF5] rounded-[3px] flex w-9 justify-center items-center">
+              <p className="text-[8px]">{data.tags[1]}</p>
+            </div>
+            <p className="text-[8px] w-9 ">
+              {parseData(data.date)}
+            </p>
+          </div>
+          
         </div>
-        
+
         <button
           type="submit"
           onClick={(e) => {
